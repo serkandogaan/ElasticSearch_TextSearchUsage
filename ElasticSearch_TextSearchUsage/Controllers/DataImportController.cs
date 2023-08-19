30b5905f6e08ad04791f3f48e2eae747a1ab8d17
serkandogaan <serkan.dogan333@outlook.com>
2023-08-19 18:00:54 +0300

    Add project files.

---
+++ ElasticSearch_TextSearchUsage/Controllers/DataImportController.cs
@@ -0,0 +1,43 @@
+﻿using ElasticSearch_TextSearchUsage.Model;
+using Faker;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+using System;
+using System.Reflection.Metadata;
+
+namespace ElasticSearch_TextSearchUsage.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataImportController : ControllerBase
+    {
+
+        [HttpPost(Name = "AddFakeText")]
+        public void AddFakeText()
+        {
+            var settings = new ConnectionSettings(new Uri("https://localhost:9200/")).BasicAuthentication("serkan", "pass123").DefaultIndex("documents-person").ServerCertificateValidationCallback((sender, certificate, chain, sslPolicyErrors) => true);
+
+            var client = new ElasticClient(settings);
+
+            var createIndexResponse = client.Indices.Create("documents-person", c => c
+    .Map<Person>(m => m
+        .AutoMap()
+    )
+);
+
+            var people = new List<Person>();
+            for (int i = 130350; i <= 1000000; i++)
+            {             
+                client.IndexDocument(new Person
+                {
+                    Id = i,
+                    Name = Faker.Name.FullName(),
+                    Description = Lorem.Sentence(10)
+                });
+            }        
+
+        }
+    }
+}
---
+++ ElasticSearch_TextSearchUsage/Controllers/SearchTextController.cs
@@ -0,0 +1,39 @@
+﻿using ElasticSearch_TextSearchUsage.Model;
+using Faker;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+using System;
+using System.Text.Json;
+
+namespace ElasticSearch_TextSearchUsage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchTextController : ControllerBase
+    {
+        
+        [HttpGet]
+        public List<Person> Search(string searchText)
+        {
+            var settings = new ConnectionSettings(new Uri("https://localhost:9200/")).BasicAuthentication("serkan", "pass123").DefaultIndex("documents-person").ServerCertificateValidationCallback((sender, certificate, chain, sslPolicyErrors) => true);
+
+            var client = new ElasticClient(settings);        
+
+            var searchResponse = client.Search<Person>(s => s
+            .Index("documents-person")
+            .Query(q => q
+                .Match(m => m
+                    .Field(f => f.Description)
+                    .Query(searchText)
+                )
+            )
+        ).Documents.ToList();
+
+
+            return searchResponse;
+
+
+        }
+    }
+}
